becea468ef692cedf55a2f980dbe2141c05dc19d
Alexandru <web2_08@iesjovellanos.org>
2021-04-26 19:06:45 +0200

    Añadido tabla de estadisticas e nueva interfaz

---
+++ server/filtro.php
@@ -0,0 +1,64 @@
+<?php
+    class filtro{       
+        private $liga;
+        private $base;
+        private $categoria="senior";
+        private $genero;
+        private $arrayArrays;
+        
+
+        
+        function __construct($liga,$base) {
+            $i = 0;
+            $this->base=$base;
+            if($liga=='SF'){
+                $this->genero = "femenino";
+                $this->liga = "superliga1";
+            }else if($liga=='SM'){
+                $this->genero = "masculino";
+                $this->liga = "superliga1";
+            }else if($liga=='SF2'){
+                $this->genero = "femenino";
+                $this->liga = "superliga2";
+            }else if($liga=='SM2'){
+                $this->genero = "masculino";
+            $this->liga = "superliga2";
+            }else if($liga=='TODO'){
+                $this->genero = "todo";
+                $this->liga = "todo";
+            }
+            if($this->genero=="todo" && $this->liga=="todo"){
+                $base->consulta("SELECT j.id_jugador, e.id_equipo, e.nombre_equipo
+                FROM statisticssports.jugador j 
+                inner join statisticssports.equipo e on j.ID_EQUIPO=e.id_equipo;");
+            }else{
+                $base->consulta("SELECT j.id_jugador, e.id_equipo, e.nombre_equipo
+                FROM statisticssports.jugador j 
+                inner join statisticssports.equipo e on j.ID_EQUIPO=e.id_equipo 
+                where e.liga='$this->liga' and e.genero='$this->genero';");
+            }
+            while ($fila = $base->extraer_registro()) {
+                foreach ($fila as $indice => $valor) {
+                    //Guardamos el NOMBRE
+                    if($indice=="id_jugador"){
+                        $array1[0]=$valor;
+                    }
+                    //Guardamos el puesto
+                    if($indice=="id_equipo"){
+                        $array1[1]=$valor;
+                    }
+                    //Guardamos el puesto
+                    if($indice=="nombre_equipo"){
+                        $array1[2]=$valor;
+                    }   
+                }
+                $this->arrayArrays[$i] = $array1;
+                $i++;
+            }
+        }
+
+        public function datosBusqueda(){
+            return $this->arrayArrays;
+        }
+    }  
+?>
--- server/servidor.php
+++ server/servidor.php
@@ -1,15 +1,40 @@
-<?php 
-    include 'jugador_class.php';
-    include 'accion_class.php';
-    include 'BD_class.php';
-    $base = new conexion("localhost","root","","statisticssports",3306,"");
-    if($_REQUEST['array']!=null){
-        $array = json_decode($_REQUEST['array']);
-        $accion = new accion($array);
-        $ideq= $accion->getEquipo();
-        $idjug= $accion->getJugador();
-        $campo= $accion->campo();
-        $porcentaje= $accion->convertir();
-        $jugador= new jugador($idjug,$ideq,$base);
-        $jugador->actualizaraccion($campo,$porcentaje,$idjug,$base,$ideq);
+<?php
+include 'jugador_class.php';
+include 'accion_class.php';
+include 'BD_class.php';
+include 'filtro.php';
+include 'utilsServidor.php';
+$base = new conexion("localhost", "root", "", "statisticssports", 3306, "");
+if (isset($_REQUEST['array'])) {
+    $array = json_decode($_REQUEST['array']);
+    $accion = new accion($array);
+    $ideq = $accion->getEquipo();
+    $idjug = $accion->getJugador();
+    $campo = $accion->campo();
+    $porcentaje = $accion->convertir();
+    $jugador = new jugador($idjug, $ideq, "", $base);
+    $jugador->actualizaraccion($campo, $porcentaje, $idjug, $base, $ideq);
+}
+if (isset($_REQUEST['filtro'])) {
+    if ($_REQUEST['filtro'] == "SF") {
+        $filtroBusqueda = $_REQUEST['filtro'];
+        $resultado = busquedas($filtroBusqueda, $base);
+        echo $resultado;
+    } else if ($_REQUEST['filtro'] == "SM") {
+        $filtroBusqueda = $_REQUEST['filtro'];
+        $resultado = busquedas($filtroBusqueda, $base);
+        echo $resultado;
+    } else if ($_REQUEST['filtro'] == "SF2") {
+        $filtroBusqueda = $_REQUEST['filtro'];
+        $resultado = busquedas($filtroBusqueda, $base);
+        echo $resultado;
+    } else if ($_REQUEST['filtro'] == "SM2") {
+        $filtroBusqueda = $_REQUEST['filtro'];
+        $resultado = busquedas($filtroBusqueda, $base);
+        echo $resultado;
+    } else if ($_REQUEST['filtro'] == "TODO") {
+        $filtroBusqueda = $_REQUEST['filtro'];
+        $resultado = busquedas($filtroBusqueda, $base);
+        echo $resultado;
     }
+}
